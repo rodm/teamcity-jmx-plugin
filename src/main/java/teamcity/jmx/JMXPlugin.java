ad6910eb08c836ad0a3ef3c2cd7c209dbf722e61
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2018-05-31 16:19:39 +0100

    Add license

--- src/main/java/teamcity/jmx/JMXPlugin.java
+++ src/main/java/teamcity/jmx/JMXPlugin.java
@@ -1,6 +1,28 @@
+/*
+ * Copyright 2018 Rod MacKenzie.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package teamcity.jmx;
 
-import jetbrains.buildServer.serverSide.*;
+import jetbrains.buildServer.serverSide.BuildServerAdapter;
+import jetbrains.buildServer.serverSide.ProjectManager;
+import jetbrains.buildServer.serverSide.SBuildAgent;
+import jetbrains.buildServer.serverSide.SBuildServer;
+import jetbrains.buildServer.serverSide.SBuildType;
+import jetbrains.buildServer.serverSide.SProject;
+import jetbrains.buildServer.serverSide.SRunningBuild;
 import jetbrains.buildServer.users.SUser;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
